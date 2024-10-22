7c63f5146fa8a7c298959856f6dc3b920e2c4f2d
Bilal Essafi <3trn873j@students.codeinstitute.net>
2024-10-22 06:26:36 +0000

    Reorganise JS file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,17 +37,3 @@ nextButton.addEventListener('click', function () {
     }
 });
 
-// Google Maps Location
-function initMap() {
-    const leopoldStr = { lat: 48.1618, lng: 11.5843 };
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: leopoldStr
-    });
-    const marker = new google.maps.Marker({
-        position: leopoldStr,
-        map: map,
-        title: "Leopoldstraße 50, Munich"
-    });
-}
-
